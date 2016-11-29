2eb39c9ba8e9587504718c726435cfe3761ffb2c
Raymond Feng <rfeng@us.ibm.com>
2016-11-29 13:44:54 -0800

    Add support for tags

--- test/codegen/swagger-v2.test.js
+++ test/codegen/swagger-v2.test.js
@@ -8,24 +8,30 @@ var V2Generator = require('../../lib/codegen/generator-v2');
 
 var petStoreV2Spec = require('../../example/pet-store-2.0.json');
 var pet2 = require('./pet-expanded.json');
+var note = require('./note.json');
 var generator = new V2Generator();
 
 describe('Swagger spec v2 generator', function() {
   it('generates remote methods', function() {
     var code = generator.generateRemoteMethods(petStoreV2Spec,
       { modelName: 'Store' });
-    expect(code).to.be.string;
+    expect(code.Store).to.be.string;
   });
 
   it('generates remote methods', function() {
     var code = generator.generateRemoteMethods(pet2,
-      { modelName: 'Pet' });
-    expect(code).contain('Pet.PetFindPets = function(tags, limit, callback)');
-    expect(code).contain('Pet.remoteMethod(\'PetFindPets\'');
+      { modelName: 'Pet' }).Pet;
+    expect(code).contain('Pet.findPets = function(tags, limit, callback)');
+    expect(code).contain('Pet.remoteMethod(\'findPets\'');
     expect(code).contain('Pet.findPetByIdId = function(id, callback)');
     expect(code).contain('Pet.remoteMethod(\'findPetByIdId\'');
     expect(code).contain('Pet.deletePet = function(id, callback)');
     expect(code).contain('Pet.remoteMethod(\'deletePet\'');
+  });
+
+  it('generates remote methods with tags', function() {
+    var code = generator.generateRemoteMethods(note, {});
+    expect(Object.keys(code)).eql(['User', 'Note']);
   });
 
   it('transform operations', function() {
